88f79bafc00f08d7d0d78d3e5395005e9268966d
Stuart Forrest <stuart.forrest@krakenflex.com>
2024-09-21 07:37:14 +0100

    Remove linting from CI until there is a working script that will run on Ubuntu, linting locally is still possible

--- scripts/lint-ci.sh
+++ scripts/lint-ci.sh
@@ -2,7 +2,7 @@
 
 ## Linting multiple files at the same time with jsonnet-lint has known bug
 ## https://github.com/google/go-jsonnet/issues/591
-find -type f \
+find . -type f \
     generator/*.jsonnet \
     generator/*.libsonnet \
     generator/**/*.jsonnet \
